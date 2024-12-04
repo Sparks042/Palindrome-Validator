efb9ddfbb04d93f5c78a765ee193721a2686d54a
sparks042 <chinedufreedom10@gmail.com>
2024-12-04 22:25:20 +0100

    updated

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const input = document.getElementById("palindrome-input")
-const button = document.getElementById("button") 
+const button = document.getElementById("button")
 
 
-button.addEventListener("click", (e) =>
-        {
-   e.preventDefault();
-   if(input.value == "" || input.value == "number"){
-    alert("Please enter a word")
-   }
-    else if (input.value === input.value.split('').reverse().join('')){
-    alert(`${input.value} is a palindrome`)
-   }
-   else{
-    alert(`${input.value} is not a palindrome`)
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input.value == "" || input.value == "number") {
+        alert("Please enter a word")
+    }
+    else if (input.value === input.value.split('').reverse().join('')) {
+        input.value = input.value.toLowerCase();
+        alert(`${input.value} is a palindrome`)
+    }
+    else {
+        alert(`${input.value} is not a palindrome`)
     }
 })
