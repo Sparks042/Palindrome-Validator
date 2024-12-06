1b7674981dddd0c9a9ac134447339c267a4763e8
sparks042 <chinedufreedom10@gmail.com>
2024-12-06 19:29:59 +0100

    updating

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const input = document.getElementById("palindrome-input")
-const button = document.getElementById("button")
-
-
+let input = document.getElementById("palindrome-input")
+let button = document.getElementById("button")
+let inputValue = input.value.toLowerCase();
+input.oninput = (e) => {
+    e.target.value.toLowerCase()
+}
 button.addEventListener("click", (e) => {
     e.preventDefault();
     if (input.value == "" || input.value == "number") {
         alert("Please enter a word")
     }
-    else if (input.value === input.value.split('').reverse().join('')) {
-        input.value = input.value.toLowerCase();
-        alert(`${input.value} is a palindrome`)
+    else if (input.value.toLowerCase() === input.value.toLowerCase().split('').reverse().join('')) {
+        // alert(`${input.value.toLowerCase()} is a palindrome`)
+        alert('Correct')
     }
     else {
-        alert(`${input.value} is not a palindrome`)
+        // alert(`${input.value.toLowerCase()} is not a palindrome`)
+        alert('Incorrect')
     }
 })
